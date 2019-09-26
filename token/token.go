f8bcd40e1b472e37e9a337c2ce5af9136260b91d
Kévin Gossé <gossekevin1@gmail.com>
2019-09-26 18:12:36 +0200

    init token package

---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+// Type identifies the type of a token
+type Type string
+
+// Token holds a token
+type Token struct {
+	Kind    Type
+	Literal string
+}
+
+// Constants for Token Types
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
