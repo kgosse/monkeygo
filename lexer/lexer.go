b416f3af8d4d87fdee86ab686c14ff86269e8c67
Kévin Gossé <gossekevin1@gmail.com>
2019-09-29 07:33:07 +0200

    update lexer.TestNextToken

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -53,6 +53,12 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		}
+		tok = newToken(token.ILLEGAL, l.ch)
 	}
 
 	l.readChar()
@@ -61,4 +67,16 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.Type, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
