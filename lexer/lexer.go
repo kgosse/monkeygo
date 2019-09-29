85458a3732769468388e30e7f22c672a1008936b
Kévin Gossé <gossekevin1@gmail.com>
2019-09-29 06:52:10 +0200

    Add lexer.New

---
+++ lexer/lexer.go
@@ -0,0 +1,26 @@
+package lexer
+
+// Lexer holds fields of struct
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+// New returns a new Lexer
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
